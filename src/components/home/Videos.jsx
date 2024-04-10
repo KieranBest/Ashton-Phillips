76968aa6e723dd1211f347d470987777a7834d37
Kieran Best <best.kieran.4595@gmail.com>
2024-04-10 12:16:29 +0100

    Change Video Source and Build

--- src/components/home/Videos.jsx
+++ src/components/home/Videos.jsx
@@ -6,8 +6,8 @@ import Carousel from "./carousel.component";
 
 export const Videos = () => {
     let videos = [
-        getImageUrl("videos/phone drop skateboard.mp4"),
-        getImageUrl("videos/phone drop skateboard.mp4"),
+        "https://bestwebdev-demotest.s3.eu-west-2.amazonaws.com/videos/phone+drop+skateboard.mp4",
+        "https://bestwebdev-demotest.s3.eu-west-2.amazonaws.com/videos/phone+drop+skateboard.mp4"
     ];
     
     return (
@@ -17,7 +17,7 @@ export const Videos = () => {
                         <h1 className="text-lg pb-1">Gallery</h1>
                         <h2 className="text-4xl  underline underline-offset-4 text-red-600">Videos</h2>
                     </div>
-                    <div className="h-96 bg-yellow-100">
+                    <div className="bg-yellow-100">
                         <Carousel slides={videos} className=" " />
                     </div>
                 </div>
