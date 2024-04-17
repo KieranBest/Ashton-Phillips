c8a91e252a00d3eff2deda76a08ab4d5ea490df1
Kieran Best <best.kieran.4595@gmail.com>
2024-04-17 13:06:49 +0100

    Fixed Page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export const NotFound = () => {
     return (
-        <div className="text-6xl flex items-center justify-center">
-            <h1>404 - Not Found</h1>
+        <div className="text-center h-[86vh] bg-gradient-to-b from-black via-zinc-800 to-black text-white overflow-x-hidden scroll-smooth font-dm">
+            <h1 className="pt-40 pb-10 text-6xl">404 - Page Not Found</h1>
+            <p className="text-2xl">Sorry this page does not exist.</p>
         </div>
     )
 }
