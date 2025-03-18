91e09b229920c083521c012d55cf15c094739b11
Kieran Best <best.kieran.4595@gmail.com>
2025-03-18 20:10:41 +0000

    fix lazy loading

--- src/components/home/Gallery.jsx
+++ src/components/home/Gallery.jsx
@@ -13,28 +13,28 @@ export const Gallery = () => {
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
                     <div className="m-2">
-                        <img className="object-cover object-top w-full h-[44vh] max-w-full rounded-lg" src={getImageUrl("28.jpg")} alt="Ashton Phillips 28" loading=" lazy" />
+                        <img className="object-cover object-top w-full h-[44vh] max-w-full rounded-lg" src={getImageUrl("28.jpg")} alt="Ashton Phillips 28" loading="lazy" />
                     </div>
                     <div className="m-2">
-                        <img className="object-cover object-center w-full h-[44vh] max-w-full rounded-lg" src={getImageUrl("32.jpg")} alt="Ashton Phillips 32" loading=" lazy" />
+                        <img className="object-cover object-center w-full h-[44vh] max-w-full rounded-lg" src={getImageUrl("32.jpg")} alt="Ashton Phillips 32" loading="lazy" />
                     </div>
                     <div className="md:m-2">
-                        <img className="object-cover object-top w-full h-[44vh] max-w-full rounded-lg hidden md:block" src={getImageUrl("4.jpg")} alt="Ashton Phillips 4" loading=" lazy" />
+                        <img className="object-cover object-top w-full h-[44vh] max-w-full rounded-lg hidden md:block" src={getImageUrl("4.jpg")} alt="Ashton Phillips 4" loading="lazy" />
                     </div>
                     <div className="md:m-2">
-                        <img className="object-cover object-top lg:object-center w-full h-[44vh] max-w-full rounded-lg hidden md:block" src={getImageUrl("30.jpg")} alt="Ashton Phillips 30" loading=" lazy" />
+                        <img className="object-cover object-top lg:object-center w-full h-[44vh] max-w-full rounded-lg hidden md:block" src={getImageUrl("30.jpg")} alt="Ashton Phillips 30" loading="lazy" />
                     </div>
                     <div className="m-2">
-                        <img className="object-cover object-center w-full h-[44vh] max-w-full rounded-lg" src={getImageUrl("29.jpg")} alt="Ashton Phillips 29" loading=" lazy" />
+                        <img className="object-cover object-center w-full h-[44vh] max-w-full rounded-lg" src={getImageUrl("29.jpg")} alt="Ashton Phillips 29" loading="lazy" />
                     </div>
                     <div className="m-2">
-                        <img className="object-cover object-top w-full h-[44vh] max-w-full rounded-lg" src={getImageUrl("40.jpg")} alt="Ashton Phillips 40" loading=" lazy" />
+                        <img className="object-cover object-top w-full h-[44vh] max-w-full rounded-lg" src={getImageUrl("40.jpg")} alt="Ashton Phillips 40" loading="lazy" />
                     </div>
                     <div className="lg:m-2">
-                        <img className="object-cover object-left w-full h-[44vh] max-w-full rounded-lg hidden lg:block" src={getImageUrl("42.jpg")} alt="Ashton Phillips 42" loading=" lazy" />
+                        <img className="object-cover object-left w-full h-[44vh] max-w-full rounded-lg hidden lg:block" src={getImageUrl("42.jpg")} alt="Ashton Phillips 42" loading="lazy" />
                     </div>
                     <div className="lg:m-2">
-                        <img className="object-cover object-top w-full h-[44vh] max-w-full rounded-lg hidden lg:block" src={getImageUrl("41.jpg")} alt="Ashton Phillips 41" loading=" lazy" />
+                        <img className="object-cover object-top w-full h-[44vh] max-w-full rounded-lg hidden lg:block" src={getImageUrl("41.jpg")} alt="Ashton Phillips 41" loading="lazy" />
                     </div>
                 </div>
 
